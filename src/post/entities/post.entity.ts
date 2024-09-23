73b83cfaa4f14075fbeb85dfa331acb6b656ba39
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-09-23 18:35:51 +0530

    Add properties to Post entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,15 @@
-export class Post {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Post{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: true }) 
+  title: string; 
+
+  @Column({nullable: true})
+  description: string;
+
+  @Column({nullable: true})
+  color: string;
+}
