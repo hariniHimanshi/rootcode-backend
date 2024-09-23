68859f3af1198e49f5ee00b370dcf1ecaffd09d7
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-09-23 20:30:25 +0530

    Refactor PostController to include endpoints

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -2,25 +2,43 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { Post as PostEntity } from './entities/post.entity';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { Comment } from './entities/comment.entity';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
+  create(@Body() createPostDto: CreatePostDto): Promise<PostEntity> {
     return this.postService.create(createPostDto);
   }
 
   @Get()
-  findAll() {
-    return this.postService.findAll();
+  findAll(): Promise<PostEntity[]> {
+    return this.postService.findAllPosts();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
+  findOne(@Param('id') id: number): Promise<PostEntity> {
+    return this.postService.findPostById(id);
   }
+
+  @Post('comment')
+  createComment(@Body() createCommentDto: CreateCommentDto): Promise<Comment> {
+    return this.postService.createComment(createCommentDto);
+  }
+
+  // @Post()
+  // create(@Body() createPostDto: CreatePostDto) {
+  //   return this.postService.create(createPostDto);
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.postService.findOne(+id);
+  // }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
