7d07ab08549dc897ca2da7c54e6f920f202337d9
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-09-23 19:16:14 +0530

    Add TypeOrmModule with Post and Comment entities for database operations

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { Comment } from './entities/comment.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, Comment])], // Import TypeOrmModule with Post and Comment entities for database operations
   controllers: [PostController],
   providers: [PostService],
 })
